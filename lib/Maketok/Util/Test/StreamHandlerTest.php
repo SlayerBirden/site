3320db0acad2fd68dcc1e876273cde5e889e172c
Oleg <olegkulik1985@gmail.com>
2014-03-16 19:57:44 +0200

    update phpunit, start streamhandler test

---
+++ lib/Maketok/Util/Test/StreamHandlerTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This is a part of Maketok Site. Licensed under GPL 3.0
+ * Please do not use for your own profit.
+ * @project store
+ * @developer Slayer slayer.birden@gmail.com maketok.com
+ */
+namespace Maketok\Util;
+
+class StreamHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWrite()
+    {
+
+    }
+
+    public function testRead()
+    {
+
+    }
+
+    public function testDelete()
+    {
+
+    }
+
+    public function testLock()
+    {
+
+    }
+
+    public function testUnlock()
+    {
+
+    }
+
+    public function testEof()
+    {
+
+    }
+}
