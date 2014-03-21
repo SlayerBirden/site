0bec947939eb1621c1df2ccf739fced2886eea71
Oleg <olegkulik1985@gmail.com>
2014-03-21 12:24:57 +0200

    add index ddl, start ddl parsing tool for tests

---
+++ lib/Maketok/App/Ddl/Test/Tool/DdlCheck.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * This is a part of Maketok Site. Licensed under GPL 3.0
+ * Please do not use for your own profit.
+ * @project store
+ * @developer Slayer slayer.birden@gmail.com maketok.com
+ */
+
+namespace Maketok\App\Ddl\Test\Tool;
+
+use Maketok\App\Site;
+
+class DdlCheck
+{
+    /**
+     * @var \Zend\Db\Adapter\Adapter
+     */
+    private $_adapter;
+
+    public function __construct()
+    {
+        $this->_adapter =  Site::getAdapter();
+    }
+
+    /**
+     * @param string $table
+     */
+    public function checkTable($table)
+    {
+        $result = $this->_adapter->query("SHOW CREATE TABLE `$table`");
+        $data = $result->current();
+        $data = explode("\n", $data);
+        $fLine = array_shift($data);
+        $lLine = array_pop($data);
+        $tableInfo = array();
+        $fLine = str_replace('CREATE TABLE `', '', $fLine);
+        preg_match('/[0-9a-z_]+/', $fLine, $matches);
+        $tableInfo['name'] = $matches[0];
+        preg_match('/ENGINE=([A-Za-z]+)/', $lLine, $matches);
+        $tableInfo['engine'] = $matches[1];
+        preg_match('/DEFAULT CHARSET=([0-9a-z]+)/', $lLine, $matches);
+        $tableInfo['default_charset'] = $matches[1];
+        foreach ($data as $row) {
+            if ((strpos('PRIMARY', $row) !== false) ||
+                (strpos('UNIQUE', $row) !== false) ||
+                (strpos('CONSTRAINT', $row) !== false)) {
+                $tableInfo['constraints'][] = $this->_parseConstraint($row);
+            } elseif ((strpos('  KEY', $row) !== false)) {
+                $tableInfo['indexes'][] = $this->_parseIndex($row);
+            } else {
+                $tableInfo['columns'][] = $this->_parseColumn($row);
+            }
+        }
+    }
+
+    /**
+     * @param string $row
+     * @return array
+     */
+    protected function _parseColumn($row)
+    {
+        $columnInfo = array();
+        preg_match('/`([a-z0-9_-]+)` ([a-z]+)\(([0-9]+)/) ([A-Z0-9a-z_-]*)/', $row, $matches);
+        $columnInfo['name'] = $matches[0];
+        $columnInfo['type'] = $matches[1];
+        $columnInfo['length'] = $matches[2];
+        $other = $matches[3];
+        if (strpos('NOT NULL', $other) !== false) {
+            $columnInfo['nullable'] = false;
+        } else {
+            $columnInfo['nullable'] = true;
+        }
+        if (strpos('AUTO_INCREMENT', $other) !== false) {
+            $columnInfo['auto_increment'] = true;
+        }
+        if (strpos('unsigned', $other) !== false) {
+            $columnInfo['unsigned'] = true;
+        }
+        if (($pos = strpos('DEFAULT', $other)) !== false) {
+            $subStr = substr($other, $pos + 8);
+            $columnInfo['default'] = substr($subStr, -2);
+        }
+    }
+
+    /**
+     * @param string $row
+     * @return array
+     */
+    protected function _parseIndex($row)
+    {
+
+    }
+
+    /**
+     * @param string $row
+     * @return array
+     */
+    protected function _parseConstraint($row)
+    {
+
+    }
+
+    /**
+     * @param string $table
+     * @param string $column
+     * @return array
+     */
+    public function checkColumn($table, $column)
+    {
+        $data = $this->_getPrepare($table);
+        foreach ($data as $row) {
+            if (strpos($column, $row) !== false) {
+                return $this->_parseColumn($row);
+            }
+        }
+        return array();
+    }
+
+    /**
+     * @param string $table
+     * @return array
+     */
+    protected function _getPrepare($table)
+    {
+        $result = $this->_adapter->query("SHOW CREATE TABLE `$table`");
+        $data = $result->current();
+        $data = explode("\n", $data);
+        array_shift($data);
+        array_pop($data);
+        return $data;
+    }
+
+    /**
+     * @param string $table
+     * @param string $index
+     * @return array
+     */
+    public function checkIndex($table, $index)
+    {
+        $data = $this->_getPrepare($table);
+        foreach ($data as $row) {
+            if (strpos($index, $row) !== false) {
+                return $this->_parseIndex($row);
+            }
+        }
+        return array();
+    }
+
+    /**
+     * @param string $table
+     * @param string $constraint
+     * @return array
+     */
+    public function checkConstraint($table, $constraint)
+    {
+        $data = $this->_getPrepare($table);
+        foreach ($data as $row) {
+            if (strpos($constraint, $row) !== false) {
+                return $this->_parseConstraint($row);
+            }
+        }
+        return array();
+    }
+}
---
+++ lib/Maketok/Util/Sql/Ddl/Index/Index.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This is a part of Maketok Site. Licensed under GPL 3.0
+ * Please do not use for your own profit.
+ * @project store
+ * @developer Slayer slayer.birden@gmail.com maketok.com
+ */
+
+namespace Maketok\Util\Sql\Ddl\Index;
+
+class Index extends AbstractIndex
+{
+    /**
+     * @var string
+     */
+    protected $specification = 'INDEX %s(...)';
+
+    /**
+     * @param  string $column
+     * @param  null|string $name
+     */
+    public function __construct($column, $name = null)
+    {
+        $this->setColumns($column);
+        $this->name = $name;
+    }
+
+    /**
+     *
+     * @return array of array|string should return an array in the format:
+     *
+     * array (
+     *    // a sprintf formatted string
+     *    string $specification,
+     *
+     *    // the values for the above sprintf formatted string
+     *    array $values,
+     *
+     *    // an array of equal length of the $values array, with either TYPE_IDENTIFIER or TYPE_VALUE for each value
+     *    array $types,
+     * )
+     *
+     */
+    public function getExpressionData()
+    {
+        $colCount = count($this->columns);
+
+        $values   = array();
+        $values[] = ($this->name) ? $this->name : '';
+
+        $newSpecTypes = array(self::TYPE_IDENTIFIER);
+        $newSpecParts = array();
+
+        for ($i = 0; $i < $colCount; $i++) {
+            $newSpecParts[] = '%s';
+            $newSpecTypes[] = self::TYPE_IDENTIFIER;
+        }
+
+        $newSpec = str_replace('...', implode(', ', $newSpecParts), $this->specification);
+
+        return array(array(
+            $newSpec,
+            array_merge($values, $this->columns),
+            $newSpecTypes,
+        ));
+    }
+}
