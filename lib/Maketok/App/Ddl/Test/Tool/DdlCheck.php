9351bd92abd62900e21a6aa87bfa5645dbe88382
Oleg <olegkulik1985@gmail.com>
2014-03-25 00:34:33 +0200

    ddlcheck tests + fixes

--- lib/Maketok/App/Ddl/Test/Tool/DdlCheck.php
+++ lib/Maketok/App/Ddl/Test/Tool/DdlCheck.php
@@ -9,6 +9,7 @@
 namespace Maketok\App\Ddl\Test\Tool;
 
 use Maketok\App\Site;
+use Zend\Db\Adapter\Adapter;
 
 class DdlCheck
 {
@@ -24,34 +25,37 @@ class DdlCheck
 
     /**
      * @param string $table
+     * @return array
      */
     public function checkTable($table)
     {
-        $result = $this->_adapter->query("SHOW CREATE TABLE `$table`");
+        $result = $this->_adapter->query("SHOW CREATE TABLE `$table`", Adapter::QUERY_MODE_EXECUTE);
         $data = $result->current();
-        $data = explode("\n", $data);
+        $data = $data->getArrayCopy();
+        $data = explode("\n", $data['Create Table']);
         $fLine = array_shift($data);
         $lLine = array_pop($data);
         $tableInfo = array();
         $fLine = str_replace('CREATE TABLE `', '', $fLine);
-        preg_match('/\S+/', $fLine, $matches);
-        $tableInfo['name'] = $matches[0];
+        preg_match('/(\S+)`/', $fLine, $matches);
+        $tableInfo['name'] = $matches[1];
         preg_match('/ENGINE=([a-zA-Z0-9]+)/', $lLine, $matches);
         $tableInfo['engine'] = $matches[1];
         preg_match('/DEFAULT CHARSET=([a-z0-9]+)/', $lLine, $matches);
         $tableInfo['default_charset'] = $matches[1];
         foreach ($data as $row) {
-            if ((strpos('PRIMARY', $row) !== false) ||
-                (strpos('UNIQUE', $row) !== false) ||
-                (strpos('CONSTRAINT', $row) !== false)) {
+            if ((strpos($row, 'PRIMARY') !== false) ||
+                (strpos($row, 'UNIQUE') !== false) ||
+                (strpos($row, 'CONSTRAINT') !== false)) {
                 $tableInfo['constraints'][] = $this->_parseConstraint($row);
-            } elseif ((strpos('  KEY', $row) !== false) ||
-                (strpos('  INDEX', $row) !== false)) {
+            } elseif ((strpos($row, '  KEY') !== false) ||
+                (strpos($row, '  INDEX') !== false)) {
                 $tableInfo['indexes'][] = $this->_parseIndex($row);
             } else {
                 $tableInfo['columns'][] = $this->_parseColumn($row);
             }
         }
+        return $tableInfo;
     }
 
     /**
@@ -62,25 +66,25 @@ class DdlCheck
     {
         $columnInfo = array();
         preg_match('/`(\S+)` (\w+)\((\d+)\) (.*)/', $row, $matches);
-        $columnInfo['name'] = $matches[0];
-        $columnInfo['type'] = $matches[1];
-        $columnInfo['length'] = $matches[2];
-        $other = $matches[3];
-        if (strpos('NOT NULL', $other) !== false) {
+        $columnInfo['name'] = $matches[1];
+        $columnInfo['type'] = $matches[2];
+        $columnInfo['length'] = $matches[3];
+        $other = $matches[4];
+        if (strpos($other, 'NOT NULL') !== false) {
             $columnInfo['nullable'] = false;
         } else {
             $columnInfo['nullable'] = true;
         }
-        if (strpos('AUTO_INCREMENT', $other) !== false) {
+        if (strpos($other, 'AUTO_INCREMENT') !== false) {
             $columnInfo['auto_increment'] = true;
         }
-        if (strpos('unsigned', $other) !== false) {
+        if (strpos($other, 'unsigned') !== false) {
             $columnInfo['unsigned'] = true;
         }
-        if (($pos = strpos('DEFAULT', $other)) !== false) {
-            $subStr = substr($other, $pos + 8);
-            $columnInfo['default'] = substr($subStr, -2);
+        if (($pos = strpos($other, 'DEFAULT')) !== false) {
+            $columnInfo['default'] = substr($other, $pos + 9, -2);
         }
+        return $columnInfo;
     }
 
     /**
@@ -90,7 +94,7 @@ class DdlCheck
     protected function _parseIndex($row)
     {
         $indexInfo = array();
-        preg_match('/^(?:KEY|INDEX) `(\S+)` \((\S+)/', trim($row), $matches);
+        preg_match('/^(?:KEY|INDEX) `(\S+)` \((\S+)\)/', trim($row), $matches);
         $indexInfo['name'] = $matches[1];
         $definition = $matches[2];
         $definition = explode(',', $definition);
@@ -109,19 +113,19 @@ class DdlCheck
     {
         $row = trim($row);
         $constraintInfo = array();
-        if (preg_match('/^(?:PRIMARY KEY|UNIQUE KEY) \((\S+)/', $row, $matches)) {
+        if (preg_match('/^(?:PRIMARY KEY|UNIQUE KEY).+\((\S+)\)/', $row, $matches)) {
             if (strpos($row, 'PRIMARY') !== false) {
                 $constraintInfo['type'] = 'primary';
             } else {
                 $constraintInfo['type'] = 'unique';
             }
-            $definition = $matches[2];
+            $definition = $matches[1];
             $definition = explode(',', $definition);
             array_walk($definition, function(&$row) {
                 $row = str_replace('`', '', $row);
             });
             $constraintInfo['definition'] = $definition;
-        } elseif (preg_match('/^CONSTRAINT `(\S+)` FOREIGN KEY \(`(\S+)`\) REFERENCES `(\S+) \(`(\S+)`\) ON DELETE (CASCADE|RESTRICT|SET NULL|NO ACTION) ON UPDATE (CASCADE|RESTRICT|SET NULL|NO ACTION)`/', $row, $matches)) {
+        } elseif (preg_match('/^CONSTRAINT `(\S+)` FOREIGN KEY \(`(\S+)`\) REFERENCES `(\S+)` \(`(\S+)`\) ON DELETE (CASCADE|RESTRICT|SET NULL|NO ACTION) ON UPDATE (CASCADE|RESTRICT|SET NULL|NO ACTION)/', $row, $matches)) {
             $constraintInfo['type'] = 'foreign_key';
             $constraintInfo['name'] = $matches[1];
             $constraintInfo['column'] = $matches[2];
@@ -142,7 +146,7 @@ class DdlCheck
     {
         $data = $this->_getPrepare($table);
         foreach ($data as $row) {
-            if (strpos($column, $row) !== false) {
+            if (strpos($row, $column) !== false) {
                 return $this->_parseColumn($row);
             }
         }
@@ -155,9 +159,10 @@ class DdlCheck
      */
     protected function _getPrepare($table)
     {
-        $result = $this->_adapter->query("SHOW CREATE TABLE `$table`");
+        $result = $this->_adapter->query("SHOW CREATE TABLE `$table`", Adapter::QUERY_MODE_EXECUTE);
         $data = $result->current();
-        $data = explode("\n", $data);
+        $data = $data->getArrayCopy();
+        $data = explode("\n", $data['Create Table']);
         array_shift($data);
         array_pop($data);
         return $data;
@@ -172,26 +177,11 @@ class DdlCheck
     {
         $data = $this->_getPrepare($table);
         foreach ($data as $row) {
-            if (strpos($index, $row) !== false) {
+            if (strpos($row, "KEY `$index") !== false) {
                 return $this->_parseIndex($row);
             }
         }
         return array();
     }
 
-    /**
-     * @param string $table
-     * @param string $constraint
-     * @return array
-     */
-    public function checkConstraint($table, $constraint)
-    {
-        $data = $this->_getPrepare($table);
-        foreach ($data as $row) {
-            if (strpos($constraint, $row) !== false) {
-                return $this->_parseConstraint($row);
-            }
-        }
-        return array();
-    }
 }
