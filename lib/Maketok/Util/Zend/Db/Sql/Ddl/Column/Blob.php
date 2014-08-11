ee48a307216840b7a58a961d69ff20318c5e87c0
SlayerBirden <olegkulik1985@gmail.com>
2014-08-11 20:06:30 +0300

    add changes to blob, text columns

--- lib/Maketok/Util/Zend/Db/Sql/Ddl/Column/Blob.php
+++ lib/Maketok/Util/Zend/Db/Sql/Ddl/Column/Blob.php
@@ -12,7 +12,7 @@ use Zend\Db\Sql\Ddl\Column\Column;
 
 /**
  * Fix Blob definitions: (as per http://dev.mysql.com/doc/refman/5.0/en/blob.html)
- * blob can not have length nor default value
+ * blob can not have default value
  *
  * @package Maketok\Util\Zend\Db\Sql\Ddl\Column
  */
@@ -20,18 +20,49 @@ class Blob extends Column
 {
 
     /**
+     * @var int
+     */
+    protected $length;
+
+    /**
      * @var string Change type to blob
      */
     protected $type = 'BLOB';
 
     /**
+     * Some of the parameters won't really taking part in expression (as of 2.4.X):
+     * default and options
+     * left for BC
+     *
      * @param null  $name
+     * @param int|null $length
      * @param bool  $nullable
+     * @param null|string $default
+     * @param array $options
      */
-    public function __construct($name, $nullable = false)
+    public function __construct($name, $length = null, $nullable = false, $default = null, array $options = array())
     {
         $this->setName($name);
+        $this->setLength($length);
         $this->setNullable($nullable);
+    }
+
+    /**
+     * @param  int $length
+     * @return self
+     */
+    public function setLength($length)
+    {
+        $this->length = $length;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLength()
+    {
+        return $this->length;
     }
 
     /**
@@ -44,11 +75,20 @@ class Blob extends Column
         $params   = array();
         $params[] = $this->name;
         $params[] = $this->type;
-
         $types = array(self::TYPE_IDENTIFIER, self::TYPE_LITERAL);
 
+        // length
+        if ($this->length) {
+            $spec    .= '(%s)';
+            $params[] = $this->length;
+            $types[]  = self::TYPE_LITERAL;
+        }
+
+        // length
         if (!$this->isNullable) {
-            $params[1] .= ' NOT NULL';
+            $spec    .= ' %s';
+            $params[] = 'NOT NULL';
+            $types[]  = self::TYPE_LITERAL;
         }
 
         return array(array(
