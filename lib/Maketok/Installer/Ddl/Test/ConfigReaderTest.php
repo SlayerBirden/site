690493911521c14cab0f1f8a93ed7b2d4c1ef2dd
SlayerBirden <olegkulik1985@gmail.com>
2014-09-08 12:14:32 +0300

    add recursiveMerge test

--- lib/Maketok/Installer/Ddl/Test/ConfigReaderTest.php
+++ lib/Maketok/Installer/Ddl/Test/ConfigReaderTest.php
@@ -8,11 +8,21 @@
 
 namespace Maketok\Installer\Ddl\Test;
 
-use Maketok\App\Site;
 use Maketok\Installer\Ddl\ConfigReader;
 
 class ConfigReaderTest extends \PHPUnit_Framework_TestCase
 {
+
+    /** @var ConfigReader */
+    public $reader;
+    /** @var \ReflectionProperty */
+    public $treeProp;
+
+    public function setUp()
+    {
+        $this->reader = new ConfigReader();
+        $this->treeProp = new \ReflectionProperty(get_class($this->reader), '_tree');
+    }
 
     /**
      * @test
@@ -44,6 +54,175 @@ class ConfigReaderTest extends \PHPUnit_Framework_TestCase
     public function testGetDependencyTree()
     {
         // TODO: Implement getDependencyTree() method.
+    }
+
+    /**
+     * @test
+     */
+    public function testRecursiveMerge()
+    {
+        $branch = [
+            'client' => 1,
+            'version' => '0.1.0',
+            'definition' => [
+                'columns' => [
+                    'id' => [
+                        'type' => 'integer',
+                    ],
+                    'code' => [
+                        'type' => 'varchar'
+                    ],
+                ],
+                'constraints' => [
+                    'primary' => [
+                        'type' => 'primaryKey',
+                        'definition' => ['id'],
+                    ],
+                ],
+            ],
+            'dependents' => [
+                [
+                    'client' => 2,
+                    'version' => '0.1.0',
+                    'definition' => [
+                        'columns' => [
+                            'id' => [
+                                'type' => 'integer',
+                            ],
+                            'code' => [
+                                'type' => 'varchar',
+                            ],
+                            'version' => [
+                                'type' => 'varchar',
+                            ],
+                        ],
+                        'constraints' => [
+                            'primary' => [
+                                'type' => 'primaryKey',
+                                'definition' => ['id'],
+                            ],
+                            'UNQ_KEY_CODE' => [
+                                'type' => 'uniqueKey',
+                                'definition' => ['code'],
+                            ],
+                        ],
+                    ],
+                    'dependents' => [
+                        [
+                            'client' => 3,
+                            'version' => '0.1.0',
+                            'definition' => [
+                                'columns' => [
+                                    'id' => [
+                                        'type' => 'integer',
+                                    ],
+                                    'code' => [
+                                        'type' => 'varchar',
+                                        'length' => 255,
+                                    ],
+                                    'version' => [
+                                        'type' => 'varchar',
+                                        'length' => 255,
+                                    ],
+                                ],
+                                'constraints' => [
+                                    'primary' => [
+                                        'type' => 'primaryKey',
+                                        'definition' => ['id'],
+                                    ],
+                                    'UNQ_KEY_CODE' => [
+                                        'type' => 'uniqueKey',
+                                        'definition' => ['code'],
+                                    ],
+                                ],
+                            ],
+                            'dependents' => [],
+                        ],
+                    ],
+                ],
+                [
+                    'client' => 4,
+                    'version' => '0.1.0',
+                    'definition' => [
+                        'columns' => [
+                            'id' => [
+                                'type' => 'integer',
+                            ],
+                            'code' => [
+                                'type' => 'varchar',
+                                'length' => 155,
+                            ],
+                        ],
+                        'constraints' => [
+                            'primary' => [
+                                'type' => 'primaryKey',
+                                'definition' => ['id'],
+                            ],
+                        ],
+                    ],
+                    'dependents' => [
+                        [
+                            'client' => 5,
+                            'version' => '0.1.0',
+                            'definition' => [
+                                'columns' => [
+                                    'id' => [
+                                        'type' => 'integer',
+                                    ],
+                                    'code' => [
+                                        'type' => 'varchar',
+                                        'length' => 155,
+                                    ],
+                                    'title' => [
+                                        'type' => 'varchar',
+                                        'length' => 255,
+                                    ],
+                                ],
+                                'constraints' => [
+                                    'primary' => [
+                                        'type' => 'primaryKey',
+                                        'definition' => ['id'],
+                                    ],
+                                ],
+                            ],
+                            'dependents' => [],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $expected = [
+            'columns' => [
+                'id' => [
+                    'type' => 'integer',
+                ],
+                'code' => [
+                    'type' => 'varchar',
+                    'length' => 155,
+                ],
+                'version' => [
+                    'type' => 'varchar',
+                    'length' => 255,
+                ],
+                'title' => [
+                    'type' => 'varchar',
+                    'length' => 255,
+                ],
+            ],
+            'constraints' => [
+                'primary' => [
+                    'type' => 'primaryKey',
+                    'definition' => ['id'],
+                ],
+                'UNQ_KEY_CODE' => [
+                    'type' => 'uniqueKey',
+                    'definition' => ['code'],
+                ],
+            ],
+        ];
+
+        $this->assertEquals($expected, $this->reader->recursiveMerge($branch));
     }
 
 }
