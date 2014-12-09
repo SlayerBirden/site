fcce8f8d86fe47fc4afb85247f128ccf0a7cd954
SlayerBirden <olegkulik1985@gmail.com>
2014-12-09 23:20:06 +0200

    resolve silly file require

--- admin/index.php
+++ admin/index.php
@@ -6,8 +6,6 @@
  * @developer Oleg Kulik slayer.birden@gmail.com maketok.com
  */
 
-require_once dirname(__DIR__) . '/lib/Maketok/App/Config.php';
-\Maketok\App\Config::loadConfig(dirname(__DIR__) . '/src/admin/config.php');
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+Maketok\App\Config::loadConfig(dirname(__DIR__) . '/src/admin/config.php');
 Maketok\App\Site::run(\Maketok\App\Site::MODE_ADMIN);
