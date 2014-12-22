bb09b734c2ae99e603c42ebc0e90d52756dc15fd
SlayerBirden <olegkulik1985@gmail.com>
2014-12-23 01:28:27 +0200

    fix origin data

--- lib/Maketok/Model/LazyObjectPropModel.php
+++ lib/Maketok/Model/LazyObjectPropModel.php
@@ -19,7 +19,7 @@ class LazyObjectPropModel implements LazyModelInterface
     /**
      * @var array
      */
-    protected $originalData;
+    protected $originalData = [];
 
     /**
      * {@inheritdoc}
